b9f6bbda14e582b7a6c74d70cf01aeb1f0600135
xiaoxiao <mlh1421@foxmail.com>
2020-04-24 13:43:33 +0800

    增加融资台账百度图表

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
     // host: 'http://192.168.1.5:7001'
-     host: 'http://192.168.1.50:8098'
-    // host: 'http://a2546250l3.wicp.vip'
+    //  host: 'http://192.168.1.50:8098'
+    host: 'http://47.105.217.237:45533'
     // host: 'http://47.105.217.237:8080'
     // host: 'http://localhost:7001'
     // host: 'http://localhost:7001'
