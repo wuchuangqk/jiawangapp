e17e9432df39cb11fe477951f3b75d9b83cb38ce
xiaoxiao <mlh1421@foxmail.com>
2020-10-16 10:33:37 +0800

    更新

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // host: 'http://192.168.0.108:8098'
   //host: 'http://314f212z32.zicp.vip'
-  host: 'http://36.153.74.234:10081'
-  // host: 'http://192.168.0.108:8098', // 甲旺
+  // host: 'http://36.153.74.234:10081'
+  host: 'http://192.168.0.108:8098', // 甲旺
 };
